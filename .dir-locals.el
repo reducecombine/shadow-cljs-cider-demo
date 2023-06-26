bf003c8f2190341dc5032028d7cf5a42c56185b1
vemv <vemv@users.noreply.github.com>
2023-06-26 21:41:09 +0200

    Customize `cider-repl-auto-detect-type`

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,4 @@
 ((nil . ((cider-default-cljs-repl . shadow)
-         (cider-shadow-default-options . "app"))))
+         (cider-shadow-default-options . "app")
+         (cider-repl-auto-detect-type nil) ;; prevents repl buffers from magically changing from cljs type to clj type, which doesn't make sense for shadow-cljs repl buffers (they're always cljs)
+         )))
